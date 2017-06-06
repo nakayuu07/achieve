2991bcdfcb076a6097c2c1aced9d2ac17ef8226e
中村勇哉 <nakayuu07@gmail.com>
2017-06-06 10:12:09 +0000

    dive9課題4

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 100.times do |n|
   email = Faker::Internet.email
   password = "password"
+  name = "name"
   User.create!(email: email,
                password: password,
                password_confirmation: password,
