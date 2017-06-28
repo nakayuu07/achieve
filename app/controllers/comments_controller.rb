b0c79ba7ff87e2b07ef8fd3304a8e73c9530764d
中村勇哉 <nakamurayuuya@nakamurayuuya-no-MacBook-Air.local>
2017-06-28 14:06:50 +0900

    コメント機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,17 +3,30 @@ class CommentsController < ApplicationController
       # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
       @comment = current_user.comments.build(comment_params)
       @blog = @comment.blog
+
       # クライアント要求に応じてフォーマットを変更
-      respond_to do |format|
-     if @comment.save
-       format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
-       # JS形式でレスポンスを返します。
-       format.js { render :index }
-     else
-       format.html { render :new }
-     end
+        respond_to do |format|
+       if @comment.save
+         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
+         # JS形式でレスポンスを返します。
+         format.js { render :index } # comments_areaの部分だけ部分的に描画する処理が書かれている
+       else
+         format.html { render :new }
+       end
+       end
    end
- end
+
+   def destroy
+     @comment = Comment.find(params[:id])
+     @blog = @comment.blog
+     @comment.destroy
+     respond_to do |format|
+     format.html { redirect_to blog_path(@blog), notice: 'コメントを削除しました。' }
+     format.js { render :index }
+   end
+   end
+
+
     private
       # ストロングパラメーター
       def comment_params
