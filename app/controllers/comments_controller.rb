dbc1625762b939fd75af0630c1b773493380fee8
中村勇哉 <nakamurayuuya@nakamurayuuya-no-MacBook-Air.local>
2017-07-05 16:43:57 +0900

    通知機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,8 @@ class CommentsController < ApplicationController
       # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
       @comment = current_user.comments.build(comment_params)
       @blog = @comment.blog
-        binding.pry
+      @notification = @comment.notifications.build(user_id: @blog.user.id )
+
 
 
       # クライアント要求に応じてフォーマットを変更
@@ -12,6 +13,15 @@ class CommentsController < ApplicationController
          format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
          # JS形式でレスポンスを返します。
          format.js { render :index } # comments_areaの部分だけ部分的に描画する処理が書かれている
+
+         unless @comment.blog.user_id == current_user.id
+          Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'comment_created', {
+            message: 'あなたの作成したブログにコメントが付きました'
+          })
+         end
+         Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'notification_created', {
+          unread_counts: Notification.where(user_id: @comment.blog.user.id, read: false).count
+        })
        else
          format.html { render :new }
        end
@@ -27,6 +37,7 @@ class CommentsController < ApplicationController
      format.js { render :index }
    end
    end
+
 
 
     private
